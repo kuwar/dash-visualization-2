962089db274d1a707fc33f2d38c51c4e1767d7a1
kuwar <kuwarsaurav21@gmail.com>
2020-06-13 16:56:44 +0200

    added candle plot

--- candle_stick.py
+++ candle_stick.py
@@ -2,8 +2,8 @@ import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.graph_objs as go
 import pandas as pd
-import dash_table as dt
 
 from navbar import Navbar
 
@@ -12,7 +12,13 @@ nav = Navbar()
 # datasets path
 filepath = 'https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv'
 # Read dataset using Pandas
-st = pd.read_csv(filepath)
+df = pd.read_csv(filepath)
+
+# Range slider options
+dates = [
+    '2015-02-17', '2015-05-17', '2015-08-17', '2015-11-17',
+    '2016-02-17', '2016-05-17', '2016-08-17', '2016-11-17', '2017-02-17'
+]
 
 body = dbc.Container(
     [
@@ -20,18 +26,53 @@ body = dbc.Container(
             [
                 dbc.Col(
                     [
-                        html.H2("Table of stock data"),
+                        html.H2("Apple stock plot"),
                         dcc.Graph(
-                            id="candle-stick-graphic"
+                            id="candle-stick-graphic",
                         )
                     ],
                     md=12
-                ),
+                )
+            ]
+        ),
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        dcc.RangeSlider(
+                            id='candle-slider-date',
+                            marks={i: dates[i] for i in range(0, 9)},
+                            min=0,
+                            max=8,
+                            value=[1, 7]
+                        )
+                    ],
+                    md=12
+                )
             ]
         )
     ],
     className="mt-4",
 )
+
+
+def plot_candle_stick(date_range=None):
+    return {
+        'data': [
+            go.Candlestick(
+                x=df['Date'],
+                open=df['AAPL.Open'],
+                high=df['AAPL.High'],
+                low=df['AAPL.Low'],
+                close=df['AAPL.Close']
+            )
+        ],
+        'layout': go.Layout(
+            title="Candle Stick",
+            xaxis={'title': "Date"},
+            yaxis={'title': "Market Fluctuation"}
+        )
+    }
 
 
 def candle_stick_chart():
--- index.py
+++ index.py
@@ -6,7 +6,7 @@ import dash_bootstrap_components as dbc
 
 from app import App, build_graph
 from homepage import Homepage
-from candle_stick import candle_stick_chart
+from candle_stick import candle_stick_chart, plot_candle_stick
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SLATE])
 
@@ -46,6 +46,20 @@ def display_page(pathname):
 def update_graph(ohlc, date):
     graph = build_graph(ohlc, date)
     return graph
+
+
+@app.callback(
+    Output('candle-stick-graphic', 'figure'),
+    [
+        Input(component_id="candle-slider-date", component_property="value")
+    ]
+)
+def update_graph(date_range):
+    print("#############")
+    print(date_range)
+    print("#############")
+    candle_graph = plot_candle_stick()
+    return candle_graph
 
 
 if __name__ == '__main__':
